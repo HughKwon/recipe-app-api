d78dc38bc645e170113b598ba2b379df7073ac4b
Hugh Kwon <hughkwon@Hughs-MBP.lan>
2024-08-05 21:54:52 -0700

    pre-deployment updates

--- scripts/run.sh
+++ scripts/run.sh
@@ -6,5 +6,12 @@ set -e
 python manage.py wait_for_db
 # collect all static files and store them in collectstatic directory
 python mange.py collectstatic --noinput
+# make migrations if necessary
+python manage.py migrate
+
+# socket 9000 --> TCP socket on port 9000
+# our NGINX server will use the TCP socket on port 9000 to connect to our app
+# app.wsgi --> run app.app.wsgi.py
+uwsgi --socket :9000 --workers 4 --master --enable-threads --module app.wsgi
 
 
